2dc8faa2b25866de9e539314ecc01d186bb4b416
sisaacs <steve.isaacs@embeddeddomain.com>
2022-06-26 09:50:02 -0700

    Added kit make segment which includes a help message for the kit.

---
+++ kit.mk
@@ -0,0 +1,20 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# 3dpmods mod kit definitions.
+#----------------------------------------------------------------------------
+
+ifeq (${MAKECMDGOALS},help-kit)
+define HelpKitMsg
+Make segment: kit.mk
+
+3dpmods
+
+This is a supported collection of mods for 3D printers.
+
+Supported mods are:
+  tx5aspm   Tronxy X5SA Pro Core XY 3d printer.
+endef
+
+export HelpKitMsg
+help-kit:
+	@echo "$$HelpKitMsg"
+endif
