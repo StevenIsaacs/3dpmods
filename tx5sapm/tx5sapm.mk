a01d3633514e0f0a554cde9ca9ae663acc05ccbd
sisaacs <steve.isaacs@embeddeddomain.com>
2023-08-05 05:36:15 -0700

    Changed variable assignments to simple assignemnts (:=).

--- tx5sapm/tx5sapm.mk
+++ tx5sapm/tx5sapm.mk
@@ -9,30 +9,30 @@ $(call Enter-Segment,mtx5sa)
 # -----
 
 # 3D CAD models.
-openscad_VERSION = dev
+openscad_VERSION := dev
 $(call Use-Segment,model/openscad)
 
 # Firmware variant.
-marlin_VERSION = 2.1.x
+marlin_VERSION := 2.1.x
 # Marlin specific definitions.
 # Which board.
-marlin_MOD_BOARD = chitu_f103
+marlin_MOD_BOARD := chitu_f103
 # The output firmware image. This differs depending upon the board.
-marlin_FIRMWARE = update.cbd
+marlin_FIRMWARE := update.cbd
 $(call Use-Segment,firmware/marlin)
 
 # Remote control software
-GW_OS = linux
-GW_OS_BOARD = opiz
-GW_OS_VARIANT = armbian
-GW_ADMIN = ${USER}
-GW_ADMIN_ID = $(shell id -u)
-GW_ADMIN_GID = $(shell id -g)
-GW_USER = ${USER}
-GW_USER_ID = ${GW_ADMIN_ID}
-GW_USER_GID = ${GW_ADMIN_GID}
-MCU_ACCESS_METHOD = ssh
-GW_APP = octoprint
+GW_OS := linux
+GW_OS_BOARD := opiz
+GW_OS_VARIANT := armbian
+GW_ADMIN := ${USER}
+GW_ADMIN_ID := $(shell id -u)
+GW_ADMIN_GID := $(shell id -g)
+GW_USER := ${USER}
+GW_USER_ID := ${GW_ADMIN_ID}
+GW_USER_GID := ${GW_ADMIN_GID}
+MCU_ACCESS_METHOD := ssh
+GW_APP := octoprint
 $(call Use-Segment,gw_app/${GW_APP})
 
 # +++++
@@ -48,19 +48,19 @@ printer.
 See the other help targets for more information.
 
 Defines (see help for more information):
-  CAD_TOOL_3DP = ${CAD_TOOL_3DP}
-  openscad_VERSION = ${openscad_VERSION}
-  FIRMWARE = ${FIRMWARE}
-  marlin_VERSION = ${marlin_VERSION}
-  marlin_MOD_BOARD = ${marlin_MOD_BOARD}
-  marlin_FIRMWARE = ${marlin_FIRMWARE}
-  GW_APP = ${GW_APP}
-  GW_ADMIN = ${GW_ADMIN}
-  GW_USER = ${GW_USER}
-  MCU_ACCESS_METHOD = ${MCU_ACCESS_METHOD}
-  GW_OS = ${GW_OS}
-  GW_OS_BOARD = ${GW_OS_BOARD}
-  GW_OS_VARIANT = ${GW_OS_VARIANT}
+  CAD_TOOL_3DP := ${CAD_TOOL_3DP}
+  openscad_VERSION := ${openscad_VERSION}
+  FIRMWARE := ${FIRMWARE}
+  marlin_VERSION := ${marlin_VERSION}
+  marlin_MOD_BOARD := ${marlin_MOD_BOARD}
+  marlin_FIRMWARE := ${marlin_FIRMWARE}
+  GW_APP := ${GW_APP}
+  GW_ADMIN := ${GW_ADMIN}
+  GW_USER := ${GW_USER}
+  MCU_ACCESS_METHOD := ${MCU_ACCESS_METHOD}
+  GW_OS := ${GW_OS}
+  GW_OS_BOARD := ${GW_OS_BOARD}
+  GW_OS_VARIANT := ${GW_OS_VARIANT}
 
 Command line goals:
   help-${mtx5sa_seg}   Display this help.
